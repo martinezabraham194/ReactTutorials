46a596b921fa3adfa725a6f0c556b04da92ce882
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-04-30 12:04:43 -0400

    Ejected application and configured webpack to enable cssmodules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name}! And I'm {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text"  onChange={props.changed} value={props.name}/>
