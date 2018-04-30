0f1c9874dc8cf2c6e47aea11b94876097f9a6061
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-04-30 11:35:03 -0400

    remove Radium complete to start global styling in CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Radium, { StyleRoot } from 'radium';
 
 import './App.css';
 import Person from './Person/Person';
@@ -51,10 +50,6 @@ class App extends Component {
         border: '1px solid blue',
         padding: '8px',
         cursor: 'pointer',
-        ':hover': {
-          backgroundColor: 'lightgreen',
-          color: 'black'
-        }
     };
 
     let persons = null;
@@ -74,10 +69,6 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'lightred',
-        color: 'black'
-      }
     }
 
     const classes = []
@@ -91,7 +82,6 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
         <div className="App">
           <h1>This is a react app!</h1>
           <p className={classes.join(' ')} >This is really working!</p>
@@ -100,9 +90,8 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
-    
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
-
     return (
-        <div className="Person" style={style}>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name}! And I'm {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text"  onChange={props.changed} value={props.name}/>
@@ -21,4 +13,4 @@ const person = (props) => {
     );
 }
 
-export default Radium(person);
+export default person;
