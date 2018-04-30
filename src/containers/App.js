047bf983f1a664a6aefafa8ed7cf8d1c15e548d9
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-04-30 16:23:48 -0400

    changed file structure and abstracted code in App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
 
@@ -43,32 +43,20 @@ class App extends Component {
 
   render() {
 
-    const style = {
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        cursor: 'pointer',
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-                    click={() => this.deletePersonHandler(index)}
-                    name={person.name} 
-                    age={person.age}
-                    key={person.id}
-                    changed={(event) => this.nameChangeHandler(event, person.id)}/>
-            })}
-          </div>
+          <Persons 
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangeHandler}/>
+        </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     const assignedClasses = []
@@ -86,7 +74,7 @@ class App extends Component {
           <h1>This is a react app!</h1>
           <p className={assignedClasses.join(' ')} >This is really working!</p>
           <button 
-            style={style}
+          className={btnClass}
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
