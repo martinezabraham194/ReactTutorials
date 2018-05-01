85a9b626c2aee84598eb49507c443f5063562e67
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-05-01 11:21:39 -0400

    testing built in component methods for component lifecycle

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,7 +7,7 @@ class Person extends Component {
     render() {
         return (
             <div className={classes.Person}>
-                <p onClick={this.this.props.click}>I'm {this.props.name}! And I'm {this.props.age} years old!</p>
+                <p onClick={this.props.click}>I'm {this.props.name}! And I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text"  onChange={this.props.changed} value={this.props.name}/>
             </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,19 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
+  constructor (props) {
+    super(props);
+    console.log('[App].js Inside Constructor', props)
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()')
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()')
+  }
+
   state = {
     persons: [
       {id: '1', name: "Abraham", age: 24},
