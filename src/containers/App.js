d076df8d4e15b3abc16c17c7fd808a73eb891dd9
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-05-01 09:29:44 -0400

    fixed variable in cockpit.js for toggle button to change colors

--- src/containers/App.js
+++ src/containers/App.js
@@ -57,7 +57,7 @@ class App extends Component {
     return (
         <div className={classes.App}>
           <Cockpit 
-          showpersons = {this.state.showPersons}
+          showPersons = {this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}/>
           {persons}
