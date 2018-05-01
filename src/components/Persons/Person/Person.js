d1b545705bab14423b6bf8641bfae44293a9a315
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-05-01 09:55:06 -0400

    changed Person.js and Persons.js into Class components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './Person.css';
 
-const person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name}! And I'm {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text"  onChange={props.changed} value={props.name}/>
-        </div>
-    );
+class Person extends Component {
+    render() {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.this.props.click}>I'm {this.props.name}! And I'm {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text"  onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
 }
 
-export default person;
+export default Person;
