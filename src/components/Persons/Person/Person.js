5b2121bea312a33298720a24aa22bedf7ffc28c5
AbrahamMartinez <martinez.abraham194@gmail.com>
2018-05-02 10:59:07 -0400

    Used ref which allows you to ref the prop in which you called it in

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -13,6 +13,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name}! And I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
+                    ref={(inp) => { this.inputElement = inp }}
                     type="text"  
                     onChange={this.props.changed} 
                     value={this.props.name}/>
